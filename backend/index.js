e537895a7288a7d1a304d980fbe0c2f98267f7bc
Vishwanath Hatti <92014314+Vishwanathhatti@users.noreply.github.com>
2025-05-19 00:25:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOption={
-    origin:'https://darling-piroshki-2ac239.netlify.app',
+    origin:'http://localhost:5173',
     credentials:true,
 }
 app.use(cors(corsOption));
